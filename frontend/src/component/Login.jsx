bbdbd40ef0829206d68fb108efea3f357041d705
Conor Dunne <conordunne@outlook.ie>
2023-01-28 00:18:20 +0900

    Add google login

--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -1,30 +1,56 @@
-import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
-import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc';
-import shareVideo from '../assets/share.mp4';
-import logo from '../assets/logowhite.png';
-
+import React from "react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin } from "@react-oauth/google";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
 
 const Login = () => {
-  return (
-    <div className='flex justify-start items-center flex-col h-screen'>
-        <div className='relative w-full h-full'>
-            <video
-            src={shareVideo}
-            type="video/mp4"
-            loop
-            controls={false}
-            muted
-            autoPlay
-            className='w-full h-full object-cover'
-            />
-        </div>
-        <div className='absolute flex flex-col justify-center items-center top-0 righ-0 left-0 bottom-0'>
-            
-        </div>
-    </div>
-  )
-}
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
 
-export default Login
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logo} width="130px" />
+        </div>
+        <div className="shadow-2xl">
+          <GoogleOAuthProvider
+            clientId= "336050469797-ahmvs4jbiqeqv8lkgkioqibc2pddlanm.apps.googleusercontent.com"
+          >
+            <GoogleLogin
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </GoogleOAuthProvider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
