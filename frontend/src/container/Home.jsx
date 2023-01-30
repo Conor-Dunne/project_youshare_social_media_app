d90512f77da419976f1efedbb3717af122a2eff4
Conor Dunne <conordunne@outlook.ie>
2023-01-30 23:26:40 +0900

    Add sidebar

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -1,24 +1,26 @@
-import React, { useState, useRef, useEffect} from 'react'
-import { HiMenu } from 'react-icons/hi';
-import { AiFillCloseCircle } from 'react-icons/ai';
-import { Link, Route, Routes } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
 
-import Sidebar from '../component/Sidebar';
-import UserProfile from '../component/UserProfile';
-import Pins from './Pins';
-import { client } from '../client';
-import logo from '../assets/logo.png';
-import { userQuery } from '../utils/data';
-
+import Sidebar from "../component/Sidebar";
+import UserProfile from "../component/UserProfile";
+import Pins from "./Pins";
+import { client } from "../client";
+import logo from "../assets/logo.png";
+import { userQuery } from "../utils/data";
 
 const Home = () => {
-
-  const [ToggleSidebar, setToggleSidebar] = useState(false);
+  const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null);
+  const scrollRef = useRef(null);
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
   console.log(userInfo);
-  
+
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
 
@@ -27,23 +29,43 @@ const Home = () => {
       console.log("me", user);
     });
   }, []);
-  
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  });
+
   return (
-    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out'>
-      <div className='hidden md:flex h-screen flex-initial'>
-        <Sidebar />
-      </div>
-      <div className='flex md:hidden flex-row'>
-        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
+    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
+    <div className="hidden md:flex h-screen flex-initial">
+      <Sidebar user={user && user} />
+    </div>
+    <div className="flex md:hidden flex-row">
+      <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
         <Link to="/">
           <img src={logo} alt="logo" className="w-28" />
         </Link>
         <Link to={`user-profile/${user?._id}`}>
-        <img src={user?.image} alt="logo" className="w-28" />
+          <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
         </Link>
       </div>
+      {toggleSidebar && (
+      <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+        <div className="absolute w-full flex justify-end items-center p-2">
+          <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
+        </div>
+        <Sidebar closeToggle={setToggleSidebar} user={user && user} />
+      </div>
+      )}
     </div>
-  )
-}
+    <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+      <Routes>
+        <Route path="/user-profile/:userId" element={<UserProfile />} />
+        <Route path="/*" element={<Pins user={user && user} />} />
+      </Routes>
+    </div>
+  </div>
+  );
+};
 
-export default Home
+export default Home;
