99817ce823bd07d89a081dbd6c8b35e43976fade
Conor Dunne <conordunne@outlook.ie>
2023-01-30 18:18:42 +0900

    Update client with env

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "3mlg45t0",
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: "production",
   apiVersion: '2021-11-16',
   useCdn: true,
-  token: "sk1DrrsYRg35UbIP4EkWWbMOxFBh9L347cVPBKBqo0zEJuGVSnw8mPsCxuruWGo96vOmzEv8bMU0yGXOjhvO5qsbGFduGuxTQjYAPGpKRpPqWA0qkhE38ayfLFfxZzQJHwenUwX8nnziNhQ8SBDL850d1fuYMQsJsyoUrSCRcD0mwSm0sErh",
+  token: import.meta.env.VITE_SANITY_TOKEN,
 });
 
 
