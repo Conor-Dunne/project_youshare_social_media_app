45809a881db86ba0347be76941e7fb2817d8a8f4
Conor Dunne <conordunne@outlook.ie>
2023-02-04 12:29:19 +0900

    Fix

--- frontend/src/component/index.js
+++ frontend/src/component/index.js
@@ -1,4 +1,4 @@
-export { default as Navbar } from './Navbar';
+export { default as NavBar } from './NavBar';
 export { default as Sidebar } from './Sidebar';
 export { default as Feed } from './Feed';
 export { default as PinDetail } from './PinDetail';
